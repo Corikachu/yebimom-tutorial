98296b5fb47c8a4d0e097ef01e9ef0036d04dc3a
dobestan <dobestan@gmail.com>
2015-02-27 23:16:35 +0900

    GET /centers/ : return all centers list

--- centers/views.py
+++ centers/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Models
+from centers.models import Center
+
+
+def center_list(request):
+    centers = Center.objects.all()
+    return render(request, "centers/list.html", {
+        'centers': centers
+    })
--- yebitutorial/urls.py
+++ yebitutorial/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'yebitutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^centers/$', 'centers.views.center_list', name='center_list'),
 )
