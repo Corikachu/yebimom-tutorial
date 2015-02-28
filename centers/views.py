22198bee3e4ddf79af777f8147eb773c5e284349
dobestan <dobestan@gmail.com>
2015-02-28 11:12:28 +0900

    POST /center/:id/delete : delete a center

--- centers/views.py
+++ centers/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
 
 # Models
 from centers.models import Center
@@ -7,6 +8,7 @@ from centers.models import Center
 from centers.forms import CenterForm
 
 
+@require_http_methods(["GET"])
 def centers_list(request):
     centers = Center.objects.all()
     return render(request, "centers/list.html", {
@@ -14,6 +16,7 @@ def centers_list(request):
     })
 
 
+@require_http_methods(["GET", "POST"])
 def center_create(request):
     form = CenterForm(request.POST or None)
     if form.is_valid():
@@ -24,6 +27,7 @@ def center_create(request):
     })
 
 
+@require_http_methods(["GET", "POST"])
 def center_update(request, pk):
     center = get_object_or_404(Center, pk=pk)
     form = CenterForm(request.POST or None, instance=center)
@@ -34,3 +38,12 @@ def center_update(request, pk):
     return render(request, "centers/form.html", {
         'form': form
     })
+
+
+@require_http_methods(["POST"])
+def center_delete(request, pk):
+    center = get_object_or_404(Center, pk=pk)
+    if request.method == "POST":
+        center.delete()
+        return redirect("centers_list")
+    return redirect("centers_list")
--- yebitutorial/urls.py
+++ yebitutorial/urls.py
@@ -9,5 +9,8 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^centers/$', 'centers.views.centers_list', name='centers_list'),
     url(r'^center/new/$', 'centers.views.center_create', name='center_create'),
-    url(r'^center/(?P<pk>\d+)/edit/$', 'centers.views.center_update', name='center_update'),
+    url(r'^center/(?P<pk>\d+)/edit/$', 'centers.views.center_update',\
+        name='center_update'),
+    url(r'^center/(?P<pk>\d+)/delete/$', 'centers.views.center_delete',\
+        name='center_delete'),
 )
